85fdb9d8b6934f92c665daefd02fa59023957e1b
VladkaLipskaja <33036348+VladkaLipskaja@users.noreply.github.com>
2019-11-16 02:49:53 +0200

    added mappings

---
+++ STARAAPP.Entities/Maps/OrderMap.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace STARAAPP.Entities
+{
+    public class OrderMap : IEntityTypeConfiguration<Order>
+    {
+        /// <summary>
+        /// Configures the entity of type <typeparamref name="TEntity" />.
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity type.</param>
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            // Primary key
+            builder.Property(t => t.ID).ValueGeneratedOnAdd();
+
+            // Properties
+            builder.ToTable("orders");
+
+            builder.Property(t => t.ID).HasColumnName("id");
+            builder.Property(t => t.Title).HasColumnName("title");
+            builder.Property(t => t.Description).HasColumnName("description");
+            builder.Property(t => t.Photo).HasColumnName("photo");
+            builder.Property(t => t.Measurements).HasColumnName("measurements");
+            builder.Property(t => t.TimeFrom).HasColumnName("timefrom");
+            builder.Property(t => t.TimeTo).HasColumnName("timeto");
+            builder.Property(t => t.Comments).HasColumnName("comments");
+            builder.Property(t => t.IsAssigned).HasColumnName("isassigned");
+            builder.Property(t => t.Cost).HasColumnName("cost");
+            builder.Property(t => t.Latitude).HasColumnName("latitude");
+            builder.Property(t => t.Longitude).HasColumnName("longitude");
+            builder.Property(t => t.WorkerCommentBefore).HasColumnName("workercommentbefore");
+            builder.Property(t => t.WorkerCommentTimeBefore).HasColumnName("workercommenttimebefore");
+            builder.Property(t => t.WorkerPhotoBefore).HasColumnName("workerphotobefore");
+            builder.Property(t => t.CompletionTime).HasColumnName("completiontime");
+            builder.Property(t => t.WorkerCommentAfter).HasColumnName("workercommentafter");
+            builder.Property(t => t.WorkerCommentTimeAfter).HasColumnName("workercommenttimeafter");
+            builder.Property(t => t.WorkerPhotoAfter).HasColumnName("workerphotoafter");
+            builder.Property(t => t.CustomerComment).HasColumnName("customercomment");
+            builder.Property(t => t.CustomerPhoto).HasColumnName("customerphoto");
+            builder.Property(t => t.CustomerMark).HasColumnName("customermark");
+            builder.Property(t => t.CustomerCommentTime).HasColumnName("customercommenttime");
+
+            //builder.HasOne(t => t.User).WithMany(t => t.HubToUsers).HasForeignKey(t => t.UserID);
+        }
+    }
+}
---
+++ STARAAPP.Entities/Maps/OrderToUserMap.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace STARAAPP.Entities
+{
+    public class OrderToUserMap : IEntityTypeConfiguration<OrderToUser>
+    {
+        /// <summary>
+        /// Configures the entity of type <typeparamref name="TEntity" />.
+        /// </summary>
+        /// <param name="builder">The builder to be used to configure the entity type.</param>
+        public void Configure(EntityTypeBuilder<OrderToUser> builder)
+        {
+            // Primary key
+            builder.HasKey(t => new { t.UserID, t.OrderID, t.TimeStart });
+
+            // Properties
+            builder.ToTable("orderstousers");
+
+            builder.Property(t => t.UserID).HasColumnName("userid");
+            builder.Property(t => t.OrderID).HasColumnName("orderid");
+            builder.Property(t => t.TimeStart).HasColumnName("timestart");
+            builder.Property(t => t.Status).HasColumnName("status");
+            builder.Property(t => t.Duration).HasColumnName("duration");
+
+            //builder.HasOne(t => t.User).WithMany(t => t.HubToUsers).HasForeignKey(t => t.UserID);
+        }
+    }
+}
